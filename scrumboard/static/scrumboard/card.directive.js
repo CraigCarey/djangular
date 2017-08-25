3b126f3344789d24660b39a83a0e9e9c94bad365
craig <sgccarey@gmail.com>
2017-08-25 22:27:51 +0100

    Added edit and delete functionality

--- scrumboard/static/scrumboard/card.directive.js
+++ scrumboard/static/scrumboard/card.directive.js
@@ -6,10 +6,37 @@
 
     function CardDirective() {
         return {
-            // where our template is being hosted
-            templateUrl: 'static/scrumboard/card.html',
-            // E for Element, meaning we can use this as an HTML element
-            restrict: 'E'
+            templateUrl: 'static/scrumboard/card.html', // where our template is being hosted
+            restrict: 'E', // E for Element, meaning we can use this as an HTML element
+            controller: ['$scope', '$http', function ($scope, $http) {
+
+                var url = '/scrumboard/cards/' + $scope.card.id + '/';
+
+                $scope.update = function () {
+                    $http.put(
+                        url,
+                        $scope.card
+                    );
+                };
+
+                $scope.delete = function () {
+                    $http.delete(url).then (
+                        function () {
+                            var cards = $scope.list.cards;
+                            cards.splice(
+                                cards.indexOf($scope.card),
+                                1
+                            );
+                        }
+                    );
+                };
+
+                // only send update 500ms after changes stop. Could be higher?
+                $scope.modelOptions = {
+                    // debounce: 500
+                    updateOn: 'blur'
+                };
+            }]
         };
     }
 })();
